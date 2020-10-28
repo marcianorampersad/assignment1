180660f6a0b7ee031411a618de9bf662146ebfd5
marcianorampersad <73587002+marcianorampersad@users.noreply.github.com>
2020-10-28 12:28:37 +0100

    Delete test.sql

--- test.sql
+++
@@ -1,5 +0,0 @@
-test=> ALTER TABLE films ADD lexemesSummary tsvector;
-ALTER TABLE
-test=> UPDATE films SET lexemesSummary = to_tsvector(Summary);
-UPDATE 5229
-test=> SELECT url FROM films WHERE lexemesSummary @@ to_tsquery('pirate');
