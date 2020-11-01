604ab7cfff4881708f911ab4f49c232431637622
marcianorampersad <73587002+marcianorampersad@users.noreply.github.com>
2020-11-01 10:37:30 +0100

    Update top50recommendations-run1IronMan.sql

--- top50recommendations-run1IronMan.sql
+++ top50recommendations-run1IronMan.sql
@@ -2,44 +2,28 @@
 
 
 /* Open map RSL & open test database */
-pi@raspberrypi:~ $ cd RSL
-pi@raspberrypi:~/RSL $ psql test
-psql (11.7 (Raspbian 11.7-0+deb10u1))
-Type "help" for help.
+cd RSL
+psql test
 
 /* Make the table and copy the data from the csv file */
-test=> CREATE TABLE films1 (url text, title text, ReleaseDate text, Distributor text, Starring text, Summary text, Director text, Genre text, Rating text, Runtime text, Userscore text, Metascore text, scoreCounts text);
+CREATE TABLE films1 (url text, title text, ReleaseDate text, Distributor text, Starring text, Summary text, Director text, Genre text, Rating text, Runtime text, Userscore text, Metascore text, scoreCounts text);
 CREATE TABLE
-test=> \copy films1  FROM '/home/pi/RSL//moviesFromMetacritic.csv' delimiter ';' csv header ;
-COPY 5229
+\copy films1  FROM '/home/pi/RSL//moviesFromMetacritic.csv' delimiter ';' csv header ;
+
 
 /* Select the film and add lexemes */
-test=> SELECT * FROM films1 where url='iron-man';
-test=> ALTER TABLE films1 ADD lexemesSummary tsvector;
-ALTER TABLE
-test=> UPDATE films1 SET lexemesSummary = to_tsvector(Summary);
-UPDATE 5229
+SELECT * FROM films1 where url='iron-man';
+ALTER TABLE films1 ADD lexemesSummary tsvector;
+UPDATE films1 SET lexemesSummary = to_tsvector(Summary);
+
 
 /* Check films with iron man in the summary */
-test=> SELECT url FROM films1 WHERE lexemesSummary @@ to_tsquery('iron+man');
-                                url                                
--------------------------------------------------------------------
- avengers-age-of-ultron
- iron-man
- iron-man-2
- iron-man-3
- natural-born-killers
- lore
- the-perfect-holiday
-(7 rows)
-
-
+SELECT url FROM films1 WHERE lexemesSummary @@ to_tsquery('iron+man');
+                                                             
 /* Create recommender system & export csv file */
-test=> ALTER TABLE films1 ADD rank float4;
-ALTER TABLE
-test=> UPDATE films1 SET rank = ts_rank(lexemesSummary,plainto_tsquery((SELECT Summary FROM films1 WHERE url='iron-man')));
-UPDATE 5229
-test=> CREATE TABLE recommendationsBasedOnSummaryField1 AS SELECT url, rank FROM films1 WHERE rank > 0.1 ORDER BY rank DESC LIMIT 50;
-SELECT 50
-test=> \copy (SELECT * FROM recommendationsBasedOnSummaryField1) to '/home/pi/RSL/top50recommendations.csv' WITH csv;
-COPY 50
+ALTER TABLE films1 ADD rank float4;
+
+UPDATE films1 SET rank = ts_rank(lexemesSummary,plainto_tsquery((SELECT Summary FROM films1 WHERE url='iron-man')));
+CREATE TABLE recommendationsBasedOnSummaryField1 AS SELECT url, rank FROM films1 WHERE rank > 0.1 ORDER BY rank DESC LIMIT 50;
+
+\copy (SELECT * FROM recommendationsBasedOnSummaryField1) to '/home/pi/RSL/top50recommendations.csv' WITH csv;
